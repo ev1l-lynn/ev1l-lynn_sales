40e016c4361bbb03aa2274f8806d98c66a0f474b
Evelynn <evelynn.geisinger@gmail.com>
2020-04-11 21:16:31 +0200

    Customer

---
+++ src/Customer.java
@@ -0,0 +1,22 @@
+
+public class Customer {
+	private int customerID;
+	private String firstName;
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+}
